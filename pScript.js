d555c618f24cfef2a7cca7441fa87cc304a15e68
anish7503 <anishchaudhary908@gmail.com>
2022-06-08 11:01:44 +0530

    Changed home background

--- pScript.js
+++ pScript.js
@@ -1,6 +1,14 @@
 // For toggle
-menutoggle = document.querySelector('.toggle');
-console.log(menutoggle)
-menutoggle.onclick = () => {
-    classList.toggle ='active';
+let menutoggle = document.querySelector("#toggle");
+let active = document.querySelector(".active");
+
+menutoggle.addEventListener('click', function(){
+    if(menutoggle.firstChild.classList.contains('fa-bars')){
+        menutoggle.firstChild.classList.replace('fa-bars','fa-times');
+    }
+    else{
+        menutoggle.firstChild.classList.replace('fa-times','fa-bars');
+    }
+    active.classList.toggle('active');
 }
+)
