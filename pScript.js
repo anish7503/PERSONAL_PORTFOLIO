14ef223b7e5bf61a6996b408539a8a6473983361
anish7503 <anishchaudhary908@gmail.com>
2022-07-17 01:55:15 +0530

    toggle fixed and button animation added

--- pScript.js
+++ pScript.js
@@ -9,13 +9,29 @@ const header = document.querySelector(".header");
 menutoggle.onclick = function(){
     if(toggle.classList.contains('fa-bars')){
         toggle.classList.replace('fa-bars','fa-times');
+
+        menutoggle.classList.toggle('active');
+        navbar.classList.toggle('active');
+        header.classList.toggle('active');
+    }
+    else{
+        toggle.classList.replace('fa-times','fa-bars');
+        menutoggle.classList.toggle('active');
+        navbar.classList.toggle('active');
+        header.classList.toggle('active'); 
+    }
+    
+}
+
+navbar.onclick = function(){
+    if(toggle.classList.contains('fa-bars')){
+        toggle.classList.replace('fa-bars','fa-times');
     }
     else{
         toggle.classList.replace('fa-times','fa-bars'); 
+        menutoggle.classList.toggle('active');
+        navbar.classList.toggle('active');
+        header.classList.toggle('active');
     }
-
-    menutoggle.classList.toggle('active');
-    navbar.classList.toggle('active');
-    header.classList.toggle('active');
+    
 }
-
